3df2d74a520a48d150036333edb658b9f5bf547f
King-Austin <nworahebuka.a@gmail.com>
2026-04-11 23:30:22 +0100

    chore: removed all CapGo infrastructure and added clean web build CI

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { CapacitorUpdater } from "@capgo/capacitor-updater";
 import App from "./App.tsx";
 import "./index.css";
-
-CapacitorUpdater.notifyAppReady();
 
 createRoot(document.getElementById("root")!).render(<App />);
